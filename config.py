db41f55f21498e6dc81bbcd5f43c4d29b72013da
Nsuku Mareana <mrnnsu001@myuct.ac.za>
2026-08-23 08:38:20 +0000

    Clean queries, robust workflow with concurrency and pull-before-push

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ BREVO_SMTP_PORT = 587
 GMAIL_SMTP_HOST = "smtp.gmail.com"
 GMAIL_SMTP_PORT = 587
 
-# ---------- Scraping Settings (Cape Town focused) ----------
+# ---------- Scraping Settings (Cape Town focused, NO food/beverage) ----------
 SEARCH_QUERIES = [
-    # Automotive Dealerships & Services
+    # Automotive & Dealerships
     "automotive companies Cape Town contact",
     "car dealership Cape Town contact email",
     "vehicle dealership Cape Town contact",
@@ -64,101 +64,6 @@ SEARCH_QUERIES = [
     "event sponsorship Cape Town"
 ]
 MAX_COMPANIES_PER_RUN = 100
-# ---------- Seed Companies (known Cape Town companies) ----------
-SEED_COMPANIES = [
-    "https://collivery.net",
-    "https://www.bex.co.za",
-    "https://www.fastway.co.za",
-    "https://truckport.co.za",
-    "https://www.milnertonnissan.co.za",
-    "https://urgentgocourier.co.za",
-    "https://www.motusselect.co.za",
-    "https://gracecouriers.co.za",
-    "https://www.sekologistics.com",
-    "https://salogistics.co.za",
-    "https://www.starwheelsct.co.za",
-    "https://thecourierguy.co.za",
-    "https://www.intertown.co.za",
-    "https://www.citisprint.co.za",
-    "https://kempston.co.za",
-    "https://www.avautos.co.za",
-    "https://millstockcars.co.za",
-    "https://www.aerospeed.co.za",
-    "https://nwlogistics.co.za",
-    "https://www.pioneerfreight.co.za",
-    "https://liebenlogistics.co.za",
-    "https://ontrackautoservices.co.za",
-    "https://smikemotors.co.za",
-    "https://automotivedynamix.co.za",
-    "https://synergyhaulage.co.za",
-    "https://tlc-logistics.co.za",
-    "https://koreanboyz.co.za",
-    "https://lastmilefast.co.za",
-    "https://www.shipchain-sa.com",
-    "https://racingtechnik.com",
-    "https://www.citisprint.co.za",
-    "https://kesselmotors.co.za",
-    "https://www.autoworks.co.za",
-    "https://harkersauto.co.za",
-    "https://frostlogistics.co.za",
-    "https://c-h-logistics.com",
-    "https://jacservdelivery.com",
-    "https://www.trianglecouriers.co.za",
-    "https://www.autoexecutive.co.za",
-    "https://driving-force.co.za",
-    "https://www.mycouriersa.co.za",
-    "https://www.concordecars.co.za",
-    "https://www.dsv.com",
-    "https://www.craneww.com",
-    "https://www.scangl.com",
-    "https://www.happyfreight.co.za",
-    "https://bridgewaterlogistics.co.za",
-    "https://www.vwtableview.co.za",
-    "https://www.scootercouriersza.co.za",
-    "https://www.capetownmotorcycles.co.za",
-    "https://bdautobodyworks.co.za",
-    "https://connectlogistics.za.com",
-    "https://www.mrd.com",
-    "https://www.m24logistics.com",
-    "https://www.motorvia.co.za",
-    "https://www.autocarriers.co.za",
-    "https://www.freightfactory.co.za",
-    "https://www.mwezatrans.co.za",
-    "https://www.titanictrucking.co.za",
-    "https://www.carefulmovers.co.za",
-    "https://www.leebotti.co.za",
-    "https://www.steinweg.com",
-    "https://www.caferemovals.co.za",
-    "https://www.airportrentals.com",
-    "https://www.avis.co.za",
-    "https://www.hertz.co.za",
-    "https://www.pacecarrental.co.za",
-    "https://www.wisemove.co.za",
-    "https://www.velocitycartransport.co.za",
-    "https://intercityautomovers.co.za",
-    "https://movemycar.co.za",
-    "https://www.gogirl-logistics.com",
-    "https://www.vdsgroup.co.za",
-    "https://www.webfleet.com",
-    "https://www.foodfitnessfactory.co.za",
-    "https://www.bonappetitmeal.co.za",
-    "https://www.primelog.co.za",
-    "https://www.cmremovals.co.za",
-    "https://www.madmacintosh.com"
-]
-# Additional seed companies for expanded industries
-SEED_COMPANIES.extend([
-    "https://www.motorsport.co.za",
-    "https://www.vehiclebranding.co.za",
-    "https://www.workshopequipment.co.za",
-    "https://www.safetygear.co.za",
-    "https://www.engineeringnews.co.za",
-    "https://www.manufacturing.co.za",
-    "https://www.sportsmarketing.co.za",
-    "https://www.energydrink.co.za"
-])
-
-
 REQUEST_TIMEOUT = 15
 DELAY_BETWEEN_REQUESTS = 1
 
